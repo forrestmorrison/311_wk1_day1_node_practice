8a560eacd1e20535332c2fe851155e4304bfcca9
forrestmorrison <forrestmorrison@gmail.com>
2022-02-07 12:42:58 -0600

    Node Practice passing all tests

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,18 +1,44 @@
 // import moment here; use this package in each function
+var moment = require('moment'); // require
 
 const today = () => {
   // write code for dates.today
+  let currentDay = new Date();
+  let dayNumber = currentDay.getDay();
+  let dayString = "";
 
+  if (dayNumber === 0) {
+    dayString = "Sunday";
+  } else if (dayNumber === 1) {
+    dayString = "Monday";
+  } else if (dayNumber === 2) {
+    dayString = "Tuesday";
+  } else if (dayNumber === 3) {
+    dayString = "Wednesday";
+  } else if (dayNumber === 4) {
+    dayString = "Thursday";
+  } else if (dayNumber === 5) {
+    dayString = "Friday";
+  } else if (dayNumber === 6) {
+    dayString = "Saturday";
+  }
+  return dayString;
 }
+
+console.log(today());
 
 const calendar = () => {
   // write code for dates.calendar
-
+  let theDate = moment().format("MMMM D, YYYY");
+  console.log(theDate);
+  return theDate;
 }
 
 const currentTime = () => {
   // write code for dates.currentTime
-
+  let theTime = moment().format("HH:mm:ss A");
+  console.log(theTime);
+  return theTime;
 }
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,24 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return  str.split(delim);
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
+  let pairsArr = [];
+  for (let i=0; i < str.length; i+=2) {
 
+    pairsArr.push(str[i].concat(str[i+1]));
+  }
+  return pairsArr;
 }
+
+console.log(pairs("stringify"));
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  let reverseString = str.split("").reverse().join("");
+  return reverseString;
 }
 
 module.exports = {
